adfce258fd66149eb14b4533c6465712148fc26e
HotoKokoa1204 <jasper93413@gmail.com>
2022-11-01 12:54:48 +0800

    update

--- js/login.js
+++ js/login.js
@@ -9,7 +9,7 @@ loginButton.addEventListener("click", fuction1 => {
 
     if (username === "user" && password === "1234") {
         alert("You have successfully logged in.");
-        window.location.assign("../homework.html")
+        window.location.assign(url('homework.html'))
     } else if (username === "nhentai" || username === "Nhentai"){
         window.location.assign("https://nhentai.net/g/"+password)
     } else if (username === "twitter" || username === "Twitter") {
